e835f8e40709f2e93f5b121447c92474d2313392
Nehasky <102262759+NervousGroove@users.noreply.github.com>
2022-08-31 16:28:25 -0300

    Add files via upload

---
+++ light/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1661973484,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/smoke br - naja.m4a",
+		"media/smoke br - naja.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
